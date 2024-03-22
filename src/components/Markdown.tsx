d4ba3ce8d02dd26b8d8558fc47f08177c4566ab6
Harvey Dela Cruz <harvey.delacruz@connectos.co>
2024-03-22 12:25:09 +0800

    added design element on Navigation and Footer sections

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -2,16 +2,17 @@ import { parse } from "marked";
 
 type MarkdownProps = {
   children: string;
+  className?: string;
 }
 
-export default function Markdown({ children }: MarkdownProps) {
+export default function Markdown({ children, className = "" }: MarkdownProps) {
   // find all source that fetch from Strapi media
   const content = children
     .replace(/]\(\/uploads\//g, `](${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}/uploads/`);
 
   return (
     <div
-      className="prose"
+      className={`prose ${className}`}
       dangerouslySetInnerHTML={{ __html: parse(content) }}
     ></div>
   )
---
+++ src/lib/contants/FALLBACK_SEO.ts
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+const FALLBACK_SEO: Metadata = {
+  title: "Close the Loop",
+  description: "Close the Loop (CtL) provides best-in-class sustainability solutions to help companies like yours enhance corporate social responsibility programs.",
+}
+
+export default FALLBACK_SEO;
