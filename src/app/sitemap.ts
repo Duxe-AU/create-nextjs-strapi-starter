a6c0bf1b4b20213767783228a39482244a53d873
Harvey Dela Cruz <harvey.delacruz@connectos.co>
2024-04-09 11:26:29 +0800

    added dynamic sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,38 +1,19 @@
+import { PageType } from "@/lib/types/common";
+import { StrapiCollectionType } from "@/lib/types/strapi";
+import { axiosClient } from "@/lib/utils/api";
+import { concatURL } from "@/lib/utils/url";
 import { MetadataRoute } from "next";
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL as string;
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: baseURL,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${baseURL}/the-core`,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${baseURL}/success-stories`,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${baseURL}/events`,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${baseURL}/faqs`,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${baseURL}/contact`,
-      lastModified: new Date(),
-      priority: 0.5,
-    },
-  ]
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { data: { data: pages } } = await axiosClient.get<StrapiCollectionType<PageType>>("/pages");
+
+  return pages.map(({ attributes: { pagePath, updatedAt } }) => ({
+    url: concatURL(baseURL, pagePath),
+    lastModified: updatedAt,
+    priority: 1,
+  })).concat([
+    // Add other custom routes here manually
+  ]);
 }
